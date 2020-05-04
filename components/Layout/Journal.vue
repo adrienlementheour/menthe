a9158504e80843e75d69fdf69b9f0858c1762fff
adrienlementheour <adrien.le.mentheour@gmail.com>
2020-05-04 19:45:04 +0200

    📱 journal

--- components/Layout/Journal.vue
+++ components/Layout/Journal.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="journal">
-        <div class="container full-content">
+        <div class="content-journal container full-content">
             <div class="wrapper-title-journal">
                 <h3 class="square-title">
                     {{ content.journalTitle }}
@@ -14,15 +14,17 @@
                     <div class="finding-number">
                         #1
                     </div>
-                    <div class="finding-title">
-                        {{ finding.title }}
-                    </div>
-                    <div class="wrapper-date-category">
-                        <div class="finding-date">
-                            {{ finding.createdAt | toClassicDate }}
+                    <div class="finding-desc">
+                        <div class="finding-title">
+                            {{ finding.title }}
                         </div>
-                        <div class="finding-category">
-                            {{ finding.category }}
+                        <div class="wrapper-date-category">
+                            <div class="finding-date">
+                                {{ finding.createdAt | toClassicDate }}
+                            </div>
+                            <div class="finding-category">
+                                {{ finding.category }}
+                            </div>
                         </div>
                     </div>
                     <div class="finding-content" v-html="finding.content" />
@@ -73,8 +75,80 @@ export default {
     font-variation-settings: 'wght' 600, 'wdth' 100;
 }
 .finding-content {
-    p {
+    /deep/ p {
         margin: 0;
+    }
+}
+
+@media (min-width: $desktop-small) {
+    .journal {
+        padding: 0;
+    }
+    .content-journal {
+        position: relative;
+        padding: #{5 * $line-height} 0;
+        &::before,
+        &::after {
+            content: '';
+            position: absolute;
+            top: 0;
+            bottom: 0;
+            border-right: 1px solid var(--tertiary);
+        }
+        &::before {
+            left: calc(#{var(--col)} * 3);
+        }
+        &::after {
+            left: calc(#{var(--col)} * 8);
+        }
+    }
+    .finding {
+        margin-top: 3 * $line-height;
+    }
+    .wrapper-title-journal,
+    .finding-desc,
+    .finding-content,
+    .finding-number {
+        padding-left: var(--gutter);
+        padding-right: var(--gutter);
+    }
+    .wrapper-title-journal {
+        width: calc(#{var(--col)} * 3);
+    }
+    .finding {
+        display: flex;
+        > div {
+            flex: 0 0 auto;
+        }
+    }
+    .finding-desc {
+        order: 1;
+        width: calc(#{var(--col)} * 3);
+    }
+    .finding-content {
+        order: 2;
+        width: calc(#{var(--col)} * 5);
+    }
+    .finding-number {
+        order: 3;
+        width: calc(#{var(--col)} * 1);
+    }
+}
+@media (min-width: $desktop) {
+    .finding-title {
+        font-size: 2.4rem;
+    }
+    .finding-date {
+        font-size: 1.4rem;
+    }
+    .finding-category {
+        font-size: 1.4rem;
+    }
+    .finding-content {
+        font-size: 1.6rem;
+    }
+    .finding-number {
+        font-size: 2.4rem;
     }
 }
 </style>
