a35dfd13aac9bbccff68e7b8fab1b90ed5ce4893
adrienlementheour <adrien.le.mentheour@gmail.com>
2020-05-22 17:38:58 +0200

    ✨ btn mode

--- components/Layout/Header.vue
+++ components/Layout/Header.vue
@@ -2,7 +2,22 @@
     <header class="main-header container full-content">
         <div class="header-title" v-html="headerData.title" />
         <div class="wrapper-mode">
-            <button type="button" class="btn-mode" />
+            <button type="button" class="btn-mode">
+                <span class="wrapper-rays">
+                    <span />
+                    <span />
+                    <span />
+                    <span />
+                    <span />
+                    <span />
+                    <span />
+                    <span />
+                    <span />
+                    <span />
+                    <span />
+                    <span />
+                </span>
+            </button>
         </div>
         <div class="column one">
             {{ headerData.columnOne }}
@@ -58,12 +73,78 @@ export default {
     font-variation-settings: 'wght' 200, 'wdth' 150;
 }
 .btn-mode {
+    position: relative;
     display: block;
     width: $line-height;
     height: $line-height;
     border-radius: 50%;
     border: 0;
     background: var(--secondary);
+    z-index: 1;
+    // &::before,
+    // &::after {
+    //     content: '';
+    //     position: absolute;
+    // }
+    // &::before {
+    //     left: 50%;
+    //     top: 0;
+    //     height: 100%;
+    //     background: red;
+    //     width: 2px;
+    //     margin-left: -1px;
+    // }
+    // &::after {
+    //     left: 0;
+    //     top: 50%;
+    //     width: 100%;
+    //     background: red;
+    //     height: 2px;
+    //     margin-top: -1px;
+    // }
+    &:hover {
+        .wrapper-rays {
+            transform: scale(0);
+        }
+    }
+    .wrapper-rays {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        display: block;
+        width: 45px;
+        height: 45px;
+        margin: -22px 0 0 -24px;
+        transform-origin: 50% 50%;
+        transform: scale(1);
+        transition: transform 0.2s ease-out;
+        pointer-events: none;
+        z-index: -1;
+        > span {
+            &::before {
+                content: '';
+                display: block;
+                width: 1px;
+                height: 100%;
+                margin: 0 1px;
+                background: var(--secondary);
+            }
+            display: block;
+            width: 3px;
+            height: 50%;
+            position: absolute;
+            top: 0;
+            left: 50%;
+
+            transform-origin: 50% 100%;
+            $nb-rays: 12;
+            @for $i from 1 through $nb-rays {
+                &:nth-child(#{$i}) {
+                    transform: rotate(#{360 / $nb-rays * $i - 360 / $nb-rays}deg);
+                }
+            }
+        }
+    }
 }
 
 @media (min-width: $desktop-small) {
@@ -96,6 +177,19 @@ export default {
         width: calc(#{var(--col)});
         padding-left: var(--gutter);
         padding-right: var(--gutter);
+    }
+}
+
+@media (prefers-color-scheme: dark) {
+    .btn-mode {
+        &:hover {
+            .wrapper-rays {
+                transform: scale(1);
+            }
+        }
+        .wrapper-rays {
+            transform: scale(0);
+        }
     }
 }
 
